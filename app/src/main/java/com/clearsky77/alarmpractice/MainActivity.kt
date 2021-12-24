73a35d979f27e3c7dd8dccc008eaa3627c553e8e
clearsky77 <clearsky3264@gmail.com>
2021-12-24 17:19:08 +0900

    AlarmServeice 생성 / 날짜와 시간을 선택할 수 있도록

--- app/src/main/java/com/clearsky77/alarmpractice/MainActivity.kt
+++ app/src/main/java/com/clearsky77/alarmpractice/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.clearsky77.alarmpractice
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clearsky77.alarmpractice.service.AlarmService
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var alarmService: AlarmService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        alarmService = AlarmService(this)
+
+        btnExactAlarm.setOnClickListener {
+            setAlarm()
+        }
+
     }
+
+    private fun setAlarm() {
+        Calendar.getInstance().apply {
+            DatePickerDialog(
+                this@MainActivity,
+                0,
+                DatePickerDialog.OnDateSetListener{_, year, month, day ->
+                    this.set(Calendar.YEAR, year)
+                    this.set(Calendar.MONTH, month)
+                    this.set(Calendar.DAY_OF_MONTH, day)
+
+                    TimePickerDialog(
+                        this@MainActivity,
+                        0,
+                        TimePickerDialog.OnTimeSetListener { _, hour, min ->
+                            this.set(Calendar.HOUR_OF_DAY, hour)
+                            this.set(Calendar.MINUTE, min)
+                        },
+                        this.get(Calendar.HOUR_OF_DAY),
+                        this.get(Calendar.MINUTE),
+                        false
+                    ).show()
+
+                },
+                this.get(Calendar.YEAR),
+                this.get(Calendar.MONTH),
+                this.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+    }
+
 }
